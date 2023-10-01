3633f448cd171b3fc24801c99505f784e0441b53
Tsvetelin91 <cv.dilov@abv.bg>
2023-10-01 16:41:00 +0300

    Array operation with rotating array to the right from pivot

---
+++ ArrayOperations/ArrayOperations/ArrayOperation.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ArrayOperations
+{
+    public class ArrayOperation
+    {
+        public static int[] RotateArray(int[] array, int pivot)
+        {
+            if (pivot < 0 || pivot >= array.Length)
+            {
+                return array; 
+            }
+
+            pivot = pivot % array.Length;
+            ReverseArray(array, 0, pivot - 1);
+            ReverseArray(array, pivot, array.Length - 1);
+            ReverseArray(array, 0, array.Length - 1);
+
+            return array;
+        }
+
+        private static void ReverseArray(int[] array, int start, int end)
+        {
+            while (start < end)
+            {
+                var temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ ArrayOperations/ArrayOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using ArrayOperations;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        var rotate = ArrayOperation.RotateArray(array, 2);
+        Console.WriteLine(2 % 5);
+
+    }
+}
