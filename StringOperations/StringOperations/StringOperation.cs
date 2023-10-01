bc35d3a3776c1d3643ac7e9ca53e3240460a39fe
Tsvetelin91 <cv.dilov@abv.bg>
2023-10-01 11:22:05 +0300

    Are anagrams operation

---
+++ StringOperations/StringOperations/StringOperation.cs
@@ -0,0 +1,32 @@
+﻿namespace StringOperations
+{
+    public static class StringOperation
+    {
+        public static string WithOutDublicates(string keyString)
+        {
+            var result = "";
+            foreach (var key in keyString.ToLower()) 
+            {
+                if (result.IndexOf(key) == -1)
+                {
+                    result += key;
+                }
+            }
+
+            return result;
+        }
+
+        public static bool AreAnagrams(string first, string second)
+        {
+            var firstChars = first.ToLower().ToCharArray();
+            var secondChars = second.ToLower().ToCharArray();
+
+            Array.Sort(firstChars);
+            Array.Sort(secondChars);
+
+            var newFirst = new string(firstChars);
+            var newSecond = new string(secondChars);
+            return newFirst == newSecond;
+        }
+    }
+}
