ceb4c5b4d599d4c85ddbd43730912596eb189c96
Tsvetelin91 <cv.dilov@abv.bg>
2023-10-31 12:37:57 +0200

    Custom stack implementation

--- Datastructures/Datastructures/Program.cs
+++ Datastructures/Datastructures/Program.cs
@@ -1,28 +1,23 @@
 ﻿using Datastructures.BinarySearchTree;
+using Datastructures.Stack;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var tree = new BinarySearchTree<int>();
-        tree.Insert(17);
-        tree.Insert(9);
-        tree.Insert(6);
-        tree.Insert(12);
-        tree.Insert(19);
-        tree.Insert(25);
-        tree.BFS();
-        Console.WriteLine();
-        tree.InOrderPrint();
-        Console.WriteLine();
-        tree.InOrderDFS();
-        Console.WriteLine();
-        tree.PostOrderPrint();
-        Console.WriteLine();
-        tree.PostOrderDFS();
-        Console.WriteLine();
-        tree.PreOrderPrint();
-        Console.WriteLine();
-        tree.PreOrderDFS();
+        var stack = new CustomStack<int>();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        foreach (var item in stack) 
+        {
+            Console.WriteLine(item);
+        }
+        var reverseStack = stack.Reverse();
+        foreach (var item in reverseStack)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
---
+++ Datastructures/Datastructures/Stack/CustomStack.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+
+namespace Datastructures.Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> items = new List<T> ();
+
+        public int Count 
+        {
+            get { return items.Count; }
+        }
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            T poppedItem = items[this.Count - 1];
+            items.RemoveAt(this.Count - 1);
+            return poppedItem;
+        }
+
+        public T Peek()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty");
+            }
+
+            return items[this.Count - 1];
+        }
+
+        public bool IsEmpty()
+        { 
+            return this.Count == 0;
+        }
+
+        public void Clear()
+        {
+            items.Clear();
+        }
+
+        public CustomStack<T> Reverse()
+        {
+            if (this.Count <= 1)
+            {
+                throw new InvalidOperationException("The stack is IsEmpty or HashCode only one element");
+            }
+            var stack = this;
+            var newStack = new CustomStack<T>();
+            while (!IsEmpty()) 
+            {
+                newStack.Push(Pop());
+            }
+
+            return newStack;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = this.Count - 1;  i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
