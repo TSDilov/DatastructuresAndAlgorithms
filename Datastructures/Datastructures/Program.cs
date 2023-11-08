e61fa94e836e77c948dbe358c35074c0dcef6ad5
Tsvetelin91 <cv.dilov@abv.bg>
2023-11-08 11:17:59 +0200

    HeapSortAlgorithm

--- Datastructures/Datastructures/Program.cs
+++ Datastructures/Datastructures/Program.cs
@@ -6,7 +6,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] arr = { 64, 25, 12, 22, 11 };
+        int[] arr = { 64, 25, 12, 22, 11, 22 };
 
         var stack = new Stack<int>();
         stack.Push(arr[0]);
@@ -14,7 +14,8 @@ internal class Program
         stack.Push(arr[2]);
         stack.Push(arr[3]);
         stack.Push(arr[4]);
-        var list = CustomSort.InsertionSortAlgorithm(arr);
+        stack.Push(arr[5]);
+        var list = CustomSort.HeapSortAlgorithm(arr);
         Console.WriteLine(string.Join(", ", list));
     }
 }
--- Datastructures/Datastructures/SortingAlgorithms/CustomSort.cs
+++ Datastructures/Datastructures/SortingAlgorithms/CustomSort.cs
@@ -45,5 +45,53 @@
 
             return list;
         }
+
+        public static List<T> HeapSortAlgorithm<T>(IEnumerable<T> collection) where T : IComparable<T>
+        {
+            var list = new List<T>(collection);
+            var numberOfItems = list.Count;
+
+            for (int i = numberOfItems / 2 - 1; i >= 0; i--)
+            {
+                Heapify(list, numberOfItems, i);
+            }
+
+            for (int i = numberOfItems - 1; i >= 0; i--)
+            {
+                T temp = list[0];
+                list[0] = list[i];
+                list[i] = temp;
+
+                Heapify(list, i, 0);
+            }
+
+            return list;
+        }
+
+        private static void Heapify<T>(List<T> list, int numberOfItems, int i) where T : IComparable<T>
+        {
+            var largest = i;
+            var left = 2 * i + 1;
+            var right = 2 * i + 2;
+
+            if (left < numberOfItems && list[left].CompareTo(list[largest]) > 0) 
+            {
+                largest = left;
+            }
+
+            if (right < numberOfItems && list[right].CompareTo(list[largest]) > 0)
+            {
+                largest = right;
+            }
+
+            if (largest != i)
+            {
+                T swap = list[i];
+                list[i] = list[largest];
+                list[largest] = swap;
+
+                Heapify(list, numberOfItems, largest);
+            }
+        }
     }
 }
